67e9b06ef448aef40f69b7aaaf10736996b58d65
Rares Vanca <raresvanca@gmail.com>
2026-03-22 20:00:37 +0200

    Use vendored protoc for build script to improve portability

--- build.rs
+++ build.rs
@@ -1,4 +1,10 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let protoc_path =
+        protoc_bin_vendored::protoc_bin_path().expect("Failed to find vendored protoc");
+    unsafe {
+        std::env::set_var("PROTOC", protoc_path);
+    }
+
     tonic_prost_build::compile_protos("proto/warpinator.proto")?;
     let out_dir = std::env::var("OUT_DIR")?;
     std::fs::rename(format!("{}/_.rs", out_dir), format!("{}/warpinator.rs", out_dir))?;
